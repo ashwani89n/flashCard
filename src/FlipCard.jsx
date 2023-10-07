b8c5dc7db1d52e7197a465364515d147f09229eb
ashwani89n <achu89n@gmail.com>
2023-10-06 23:14:22 -0400

    This is my second commit

--- src/FlipCard.jsx
+++ src/FlipCard.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
 import './FlipCard.css';
 
-const ques = ['What does the term "sustainable agriculture" refer to?',
+let ques = ['What is the practice of cultivating crops without the use of synthetic chemicals?',
                   'What is the key principle of sustainable agriculture?',
-                  'What is the purpose of using cover crops in sustainable agriculture?',
+                  'What is the process of rotating crops to improve soil fertility and prevent soil?',
                   'Which farming method focuses on integrating plants and animals to create a self-sustaining ecosystem?',
                   'What is the term for a system of farming that does not rely on synthetic chemicals or genetically modified organisms (GMOs)?',
-                  'How does sustainable agriculture contribute to climate change mitigation?',
-                  'What is the primary goal of agroforestry?',
+                  'What is the term for rearing and raising livestock in a humane and environmentally friendly manner?',
+                  'What is the term for the conservation of water resources in agriculture?',
                   'Which sustainable farming practice involves using minimal water to grow crops efficiently?',
                   'In sustainable agriculture, what is the term for the practice of saving seeds from one harvest to plant in the next season?',
-                  'What role do pollinators like bees and butterflies play in sustainable agriculture?'];
+                  'What is the practice of using natural predators to control pests in agriculture?'];
 
-const ans = ['Sustainable agriculture refers to farming practices that aim to protect the environment and ensure the long-term viability of food production.',
-              'Biodiversity conservation',
-             'Cover crops are used to protect and enrich the soil, prevent erosion, and suppress weeds between main crop plantings.',
+let ans = ['Organic',
+             'Biodiversity conservation',
+             'Crop Rotation',
              'Agroecology',
              'Organic farming',
-             'Sustainable agriculture can sequester carbon in the soil, reducing greenhouse gas emissions and helping combat climate change',
-             'Agroforestry combines trees or shrubs with crops or livestock to enhance land productivity, biodiversity, and environmental sustainability',
-             'Drip irrigation or water-efficient irrigation methods',
-             'Seed saving or seed conservation',
-             'Pollinators are essential for the reproduction of many crops, contributing to higher yields and biodiversity in sustainable farming systems'];
+             'Pasture',
+             'Irrigation',
+             'Drip irrigation',
+             'Seed conservation',
+             'Biological Control'];
+
+
 
 function generateRandomNumber(){
 const randomNumber = Math.random() * 10;
@@ -29,11 +31,15 @@ const randomInteger = Math.floor(randomNumber);
 return randomInteger;
 }
 
-
 function FlipCard(props) {
 
 const [isFlipped, setIsFlipped] = useState(false);
 const [quesNo, setQuesNo] = useState(0);
+const [currentStreak, setCurrentStreak] = useState(0);
+const [longestStreak, setLongestStreak] = useState(0);
+const [userAnswer, setUserAnswer] = useState('');
+const [correctAnswer, setCorrectAnswer] = useState(false);
+const [quesAnswered, setQuesAnswered] = useState(false);
 
 const handleClick = () => {
     setIsFlipped(!isFlipped);
@@ -41,12 +47,84 @@ const handleClick = () => {
 
 const nextQues = () => {
     setIsFlipped(false);
-    setQuesNo(generateRandomNumber());
+    if (quesNo < ques.length - 1) {
+        setQuesNo(quesNo + 1);
+        setUserAnswer('');
+        setQuesAnswered(false);
+    }
 };
-  
+
+const PrevQues = () => {
+    setIsFlipped(false);
+    if (quesNo > 0) {
+        setQuesNo(quesNo - 1);
+        setUserAnswer('');
+        setQuesAnswered(false);
+    }
+};
+
+const shuffleQues = () => {
+    const shuffledQues = [...ques];
+    const shuffledAns = [...ans];
+
+    for (let i = shuffledQues.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+      
+        [shuffledQues[i], shuffledQues[j]] = [shuffledQues[j], shuffledQues[i]];
+        [shuffledAns[i], shuffledAns[j]] = [shuffledAns[j], shuffledAns[i]];
+    }
+
+    setQuesNo(generateRandomNumber); 
+    setCurrentStreak(0); 
+    setLongestStreak(0); 
+    setUserAnswer(''); 
+    setIsFlipped(false); 
+    ques = shuffledQues;
+    ans = shuffledAns;
+};
+
+const onCheckAnswer = () => {
+    if (userAnswer.trim() === '') {
+        console.log("not answered");
+        setQuesAnswered(false);
+        return;
+    }
+    if (userAnswer.toUpperCase() == ans[quesNo].toUpperCase()) {
+        if(isFlipped)
+        {
+            setQuesAnswered(false);
+            return;
+        } 
+        setCurrentStreak(currentStreak + 1);
+        setLongestStreak(0);
+        setCorrectAnswer(true);
+        } 
+        else {
+        if(currentStreak > longestStreak)
+            setLongestStreak(currentStreak);
+        else
+            setLongestStreak(longestStreak);
+        setCurrentStreak(0);
+        setCorrectAnswer(false);
+        }
+        setQuesAnswered(true);
+};
+
+const handleChange = (e) => {
+    const newValue = e.target.value;
+    setUserAnswer(newValue);
+  };
+
   return (
     <>
     <table align='center'>
+        <tr>
+            <td>
+                <div className='countHeader'>
+                    <h4>Current Streak:{currentStreak}, Longest Streak:{longestStreak}</h4>
+                </div> 
+            </td>
+        </tr>
         <tr>
             <td>
                 <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
@@ -55,8 +133,18 @@ const nextQues = () => {
             </td>
         </tr>
         <tr>
+            <td align='center'>
+            Guess the answer here: <input type="text" name="inputtext" value={userAnswer} className={`textBox ${quesAnswered ? (correctAnswer ? "Correct" : "Incorrect") : ""}`} placeholder="Place your answer here..." onChange={handleChange} />&nbsp;
+            <button type="submit" className="button submit" onClick={onCheckAnswer}>Submit Guess</button>
+            </td>
+        </tr>
+        <br></br>
+        <br></br>
+        <tr >
             <td>
-                <button onClick={nextQues}><img src = 'src/assets/next.png' /></button>
+                <button className="prevNext" onClick={PrevQues} disabled={quesNo === 0}><img src = 'src/assets/previous.png' /></button>&nbsp;
+                <button className="prevNext"  onClick={nextQues} disabled={quesNo === ques.length - 1}><img src = 'src/assets/next.png' /></button>&nbsp;
+                <button className="shuffle" onClick={shuffleQues}>Shuffle Questions</button>
             </td>
         </tr>
     </table>
